9ff89ea871286053cd967baf2c46b394481bb6c6
Jonatan Deporte <jonatandeporte@gmail.com>
2023-10-18 21:59:42 -0300

    sin cambios - commit previo a nuevo pull

--- src/components/CategoriasRodados/CategoriasRodados.js
+++ src/components/CategoriasRodados/CategoriasRodados.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "./categoriasRodados.css";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
 export const CategoriasRodados = () => {
-	
-	const [categorias,setCategorias] = useState([]) 
+	const [categorias, setCategorias] = useState([]);
 
-	useEffect(()=>{
-		fetch('http://localhost:3010/api/categories')
-		.then(response => response.json())
-		.then(data => {
-			setCategorias(data)
-		})
-	},[])
-	
+	useEffect(() => {
+		fetch("http://localhost:3010/api/categories")
+			.then((response) => response.json())
+			.then((data) => {
+				setCategorias(data);
+			});
+	}, []);
+
 	return (
 		<div>
 			<div class=" mb-4 pe-0">
@@ -23,15 +22,13 @@ export const CategoriasRodados = () => {
 					</div>
 					<div class="card-body ">
 						<div class="row">
-							{categorias.map((cat,i) => 
-							
-							<div class="col-lg-6 mb-4">
-								<div class="color-back card  text-white shadow ">
-									<div class="card-body">{cat.nombre}</div>
+							{categorias.map((cat, i) => (
+								<div class="col-lg-6 mb-4">
+									<div class="color-back card  text-white shadow ">
+										<div class="card-body">{cat.nombre}</div>
+									</div>
 								</div>
-							</div> 
-							
-							)}		
+							))}
 						</div>
 					</div>
 				</div>
